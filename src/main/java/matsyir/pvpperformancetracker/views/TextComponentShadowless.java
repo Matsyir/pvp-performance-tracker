cc3ec1c95927e0ff8d9d21cf1199b8e82a79bea2
Matsyir <contact@matsyir.com>
2025-11-23 21:19:56 -0500

    TextComponentShadowless - Include copyright header

--- src/main/java/matsyir/pvpperformancetracker/views/TextComponentShadowless.java
+++ src/main/java/matsyir/pvpperformancetracker/views/TextComponentShadowless.java
@@ -1,8 +1,27 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
+/*
+ * Copyright (c) 2025, Matsyir <https://github.com/Matsyir>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package matsyir.pvpperformancetracker.views;
 
 import java.awt.Color;
@@ -17,10 +36,9 @@ import javax.annotation.Nullable;
 
 import lombok.Setter;
 import net.runelite.client.ui.overlay.RenderableEntity;
-import net.runelite.client.ui.overlay.components.TextComponent;
 
 // initially copied from TextComponent.class
-// could have just extended TextComponent & only overidden renderText, but can't override renderText ._.
+// could have just extended TextComponent & only overridden renderText, but can't override renderText ._.
 public class TextComponentShadowless implements RenderableEntity {
     private static final Pattern COL_TAG_PATTERN = Pattern.compile("<col=([0-9a-fA-F]{2,6})>");
     @Setter
